5ccffce42c9345298ee27216ec85a23a03b4aab9
Manuel Morales <morales.jmanuel16@gmail.com>
2017-11-17 22:34:55 -0500

    added included hooks for invitations and invitees

--- lib/inviter/acts_as_invitation.rb
+++ lib/inviter/acts_as_invitation.rb
@@ -1,5 +1,15 @@
 module Inviter
   module ActsAsInvitation
     extend ActiveSupport::Concern
+    class << self
+      attr_reader :invitations
+    end
+
+    private
+
+    def self.included(base)
+      @invitations ||= []
+      @invitations << base
+    end
   end
 end
--- lib/inviter/acts_as_invitee.rb
+++ lib/inviter/acts_as_invitee.rb
@@ -1,5 +1,15 @@
 module Inviter
   module ActsAsInvitee
     extend ActiveSupport::Concern
+    class << self
+      attr_reader :invitees
+    end
+
+    private
+
+    def self.included(base)
+      @invitees ||= []
+      @invitees << base
+    end
   end
 end
