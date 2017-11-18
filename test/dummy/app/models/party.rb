08b847ef8c095b4fc97638a1e002c98292319375
Manuel Morales <morales.jmanuel16@gmail.com>
2017-11-18 18:24:31 -0500

    added has_many relations for users and parties through user_party

--- test/dummy/app/models/party.rb
+++ test/dummy/app/models/party.rb
@@ -1,3 +1,5 @@
 class Party < ApplicationRecord
   include Inviter::ActsAsInvitation
+  has_many :user_parties
+  has_many :users, through: :user_parties
 end
--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   include Inviter::ActsAsInviter
   include Inviter::ActsAsInvitee
+
+  has_many :user_parties
+  has_many :parties, through: :user_parties
 end
