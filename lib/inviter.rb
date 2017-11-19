d8c6c06b1314b9255db7a449da65e829987fd4c2
Manuel Morales <morales.jmanuel16@gmail.com>
2017-11-18 23:10:14 -0500

    include inviterable concern

--- lib/inviter.rb
+++ lib/inviter.rb
@@ -3,5 +3,6 @@ module Inviter
   require 'inviter/acts_as_invitation'
   require 'inviter/acts_as_invitee'
   require 'inviter/acts_as_inviter'
+  require 'app/models/concerns/inviterable'
   require 'app/models/invitation'
 end
